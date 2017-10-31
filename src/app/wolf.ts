33b64c9fbd6851a2927b8333db794b700280dd67
Theophile <theophile@iMac-de-administrador-3.local>
2017-10-31 12:00:27 +0100

    add wolf.ts

--- src/app/wolf.ts
+++ src/app/wolf.ts
@@ -1,12 +1,17 @@
 export class Wolf {
-  	name : string; 
-  	password : string;
-	cover : string; 
-	profilePic : string; 
-	description: string; 
-	infos : string; 
-	gigs : string;
-	age : number;
+ //  	name : string; 
+ //  	password : string;
+	// cover : string; 
+	// profilePic : string; 
+	// description: string; 
+	// infos : string; 
+	// gigs : string;
+	// age : number;
 	id : any;
+	firstName : string;
+	lastName : string;
+	email: string;
+	country: string;
+	updated: string;
 
 }
